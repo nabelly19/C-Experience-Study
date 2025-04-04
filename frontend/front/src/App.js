387538591acb5dea24065a44986e06174f734f3c
anabelly <anabellymontibeller@gmail.com>
2025-04-03 22:40:18 -0300

    add: adding list page

--- frontend/front/src/App.js
+++ frontend/front/src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { Route, Routes } from "react-router-dom";
 
 //Pages
+import ListPage from './pages/ListItemPage';
 
 //Components
 import NavbarComponent from './components/NavbarComponent';
@@ -11,6 +12,9 @@ function App() {
   return (
     <>
       <NavbarComponent />
+      <Routes>
+        <Route path="/" element={<ListPage />} />
+      </Routes>
     </>
   );
 }
--- frontend/front/src/pages/ListItemPage/index.js
+++ frontend/front/src/pages/ListItemPage/index.js
@@ -1,13 +1,21 @@
 import styles from "./style.module.css"
 
-export default function ListPage(){
-    return(
+export default function ListPage() {
+    return (
         <>
             <main>
-                <section class="section1">
-                    
+                <section className={styles.section1}>
+                    <div className={styles.container}>
+                        <h1 className={styles.title}>Lista Geral</h1>
+                        <h5 className={styles.subtile}>Consulte todos os personagens existentes</h5>
+                    </div>
+
+                    <div className={styles.search_bar}>
+                        
+                    </div>
+
                 </section>
-                <section class="section2">
+                <section className={styles.section2}>
 
                 </section>
             </main>
